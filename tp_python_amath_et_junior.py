9d1b63bf9e6760ada5eb4faaeb403bd421ad94f3
Amath Thiare <thiarepapi@gmail.com>
2024-12-09 23:58:37 +0100

    Ajout de notre travail

---
+++ tp_python_amath_et_junior.py
@@ -0,0 +1,42 @@
+class Creation_compte:
+    print("Pour créer un compte, entrez les informations suivantes: ")
+    prenom = input("Entrez votre prenom: ")
+    nom = input("Entrez votre nom: ")
+    print("Parfait Monsieur " + nom + ", votre compte a été créé avec succès.")
+    
+class CompteBancaire:
+    def __init__(self, prenom, nom, numero, solde):
+        self.prenom = prenom
+        self.nom = nom
+        self.numero = numero
+        self.__solde = solde
+        print("Que souhaitez-vous faire ?")
+        print("1. Afficher les informations du compte")
+        print("2. Déposer de l'argent")
+        print("3. Retirer de l'argent")
+        print("4. Afficher le solde du compte")
+
+    def action(self, montant):
+        choix = int(input())
+        if choix == 1:
+          print(f"Nom du titulaire: {self.prenom} {self.nom}, Numero du compte: {self.numero}, Solde du compte: {self.__solde}€")
+
+        elif choix == 2:
+            int(input("Entrez le montant: "))
+            self.__solde += montant
+            print(f"Votre nouveau solde est de {self.__solde}€")
+            
+        elif choix == 3:
+            int(input("Entrez le montant: "))
+            self.__solde -= montant
+            if self.__solde >= montant:
+                print(f"Votre nouveau solde est de {self.__solde}€")
+            elif self.__solde < montant:
+                print(f"Vous ne pouvez pas retirer {self.__solde}€")
+
+        elif choix == 4:
+            self.__solde = self.__solde
+            print(f"Le solde de votre compte est de {self.__solde}€")
+            
+compte = CompteBancaire("Amath", "Thiare", 4830917843038596, 0)
+compte.action(int(input()))
